16d2bc581406d3de877f179ba2915bee4a3b54e8
hugogatti <hugo.gatti@hotmail.com>
2025-02-12 14:31:48 -0300

    feat: Implementação do frontend com os components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import Header from "./components/Header";
+import Main from "./components/Main";
 
-function App() {
+const App = () => {
   return (
     <>
-      
+      <Header />
+      <Main />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="main__texts">
+        <h2>Artistas Populares</h2>
+        <a className="main__link" href="/">Mostrar Tudo</a>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
