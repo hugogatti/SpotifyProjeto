9d2693e17f823c2f3f9606d411ee354f4a75ef60
hugogatti <hugo.gatti@hotmail.com>
2025-02-12 15:24:48 -0300

    feat: implementação de novos itens e funcionalidades do front

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,409 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+
 const Main = () => {
   return (
     <div className="main">
-      <div className="main__texts">
-        <h2>Artistas Populares</h2>
-        <a className="main__link" href="/">Mostrar Tudo</a>
+      <div className="item-list">
+        <div className="item-list__header">
+          <h2>Artistas Populares</h2>
+          <a className="item-list__link" href="/">
+            Mostrar Tudo
+          </a>
+        </div>
+
+        <div className="item-list__container">
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab6761610000517499c26e8fb91634b00764bbb9"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">MC Tuto</p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab6761610000517499c26e8fb91634b00764bbb9"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">MC Tuto</p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab6761610000517499c26e8fb91634b00764bbb9"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">MC Tuto</p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab6761610000517499c26e8fb91634b00764bbb9"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">MC Tuto</p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab6761610000517499c26e8fb91634b00764bbb9"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">MC Tuto</p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="item-list">
+        <div className="item-list__header">
+          <h2>Músicas Populares</h2>
+          <a className="item-list__link" href="/">
+            Mostrar Tudo
+          </a>
+        </div>
+
+        <div className="item-list__container">
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab67616d00001e025bb6668a856035550a66fc31"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Como É Que A Gente Fica - Ao Vivo
+                </p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab67616d00001e025bb6668a856035550a66fc31"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Como É Que A Gente Fica - Ao Vivo
+                </p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab67616d00001e025bb6668a856035550a66fc31"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Como É Que A Gente Fica - Ao Vivo
+                </p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab67616d00001e025bb6668a856035550a66fc31"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Como É Que A Gente Fica - Ao Vivo
+                </p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab67616d00001e025bb6668a856035550a66fc31"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Como É Que A Gente Fica - Ao Vivo
+                </p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab67616d00001e025bb6668a856035550a66fc31"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Como É Que A Gente Fica - Ao Vivo
+                </p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab67616d00001e025bb6668a856035550a66fc31"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Como É Que A Gente Fica - Ao Vivo
+                </p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab67616d00001e025bb6668a856035550a66fc31"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Como É Que A Gente Fica - Ao Vivo
+                </p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab67616d00001e025bb6668a856035550a66fc31"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Como É Que A Gente Fica - Ao Vivo
+                </p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+
+          <div className="single-item">
+            <div className="single-item__div-image-button">
+              <div className="single-item__div-image">
+                <img
+                  className="single-item__image"
+                  src="https://i.scdn.co/image/ab67616d00001e025bb6668a856035550a66fc31"
+                  alt="Artista X"
+                />
+              </div>
+
+              <FontAwesomeIcon
+                className="single-item__icon"
+                icon={faCirclePlay}
+              />
+            </div>
+
+            <div className="single-item__texts">
+              <div className="single-item__2lines">
+                <p className="single-item__title">
+                  Como É Que A Gente Fica - Ao Vivo
+                </p>
+              </div>
+              <p className="single-item__type">Artista</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
