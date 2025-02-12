0e793b0476ca0821734c1352206f9b5fa6f8966d
hugogatti <hugo.gatti@hotmail.com>
2025-02-12 17:02:53 -0300

    feat: Implementação para enviar, receber e consumir parametros

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,36 @@
+import SingleItem from "./SingleItem";
+
+const ItemList = ({ title, items, itemsArray, path, idPath }) => {
+  return (
+    <div className="item-list">
+      <div className="item-list__header">
+        <h2>{title} Populares</h2>
+        <a href={path} className="item-list__link">
+          Mostrar Tudo
+        </a>
+      </div>
+
+      <div className="item-list__container">
+        {itemsArray
+          .filter((currentValue, index) => index < items)
+          .map((currObj, index) => {
+            return (
+              <SingleItem
+                idPath={idPath}
+                //método para consumir
+                //id={currObj.id}
+                //name={currObj.name}
+                //image={currObj.image}
+                //banner={currObj.banner}
+                //método para consumir
+                {...currObj}
+                key={`${title}-${index}`}
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default ItemList;
