2a1cade13e40dd935a8d044c09b988dfc9631746
hugogatti <hugo.gatti@hotmail.com>
2025-02-12 22:51:01 -0300

    chore: Outras mudanças que não modificam arquivos de código-fonte

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,8 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
+      'no-unused-vars': 'off',
+      'react/prop-types': 'off',
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
