2cfbb2e0acdd5db892b25384a277ddf097cef3c4
Hugo Gatti <67435861+hugogatti@users.noreply.github.com>
2025-02-14 15:00:07 -0300

    feat: informações do seu Banco de dados

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -2,9 +2,9 @@
 import { MongoClient } from "mongodb";
 
 const URI =
-  "mongodb+srv://dbspotify:Hg230497-Bc@cluster0.0sita.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  "mongodb+srv://<NomeUsuario>:<Senha>@cluster0.0sita.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 //usar o new por ser uma classe
 const client = new MongoClient(URI);
 
-export const db = client.db("spotifyProjeto");
+export const db = client.db("NomeDaSuaDB");
